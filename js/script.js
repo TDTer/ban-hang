d643e556ff7518f89be80e317988d6d64a249775
Tran Tran <trantth@blockchainlabs.asia>
2023-08-11 20:44:23 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    speed: 1000,
+    spaceBetween: 20,
+    pagination: {
+       el: ".swiper-pagination",
+       clickable:true,
+     },
+     autoplay: {
+       delay: 2000,
+       disableOnInteraction: false,
+    },
+ });
+ 
+  var swiper = new Swiper(".category-slider", {
+    loop:true,
+    speed: 1000,
+    spaceBetween: 20,
+    pagination: {
+       el: ".swiper-pagination",
+       clickable:true,
+    },
+    autoplay: {
+       delay: 2000,
+       disableOnInteraction: false,
+    },
+    breakpoints: {
+       0: {
+          slidesPerView: 2,
+        },
+       650: {
+         slidesPerView: 3,
+       },
+       768: {
+         slidesPerView: 4,
+       },
+       1024: {
+         slidesPerView: 5,
+       },
+    },
+ });
+ 
+ var swiper = new Swiper(".products-slider", {
+    loop:true,
+    spaceBetween: 20,
+    pagination: {
+       el: ".swiper-pagination",
+       clickable:true,
+    },
+    breakpoints: {
+       550: {
+         slidesPerView: 2,
+       },
+       768: {
+         slidesPerView: 2,
+       },
+       1024: {
+         slidesPerView: 3,
+       },
+    },
+ });
